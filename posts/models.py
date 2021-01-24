34f0b492cfafa101b3e4775a7c92f988c623ad57
Михаил <Mikevlg34@yandex.ru>
2021-01-24 23:19:25 +0400

    This is the way

--- posts/models.py
+++ posts/models.py
@@ -5,12 +5,13 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(verbose_name="Текст")
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="posts")
+                               related_name="posts", verbose_name="Автор")
     group = models.ForeignKey('Group', on_delete=models.SET_NULL, blank=True,
-                              null=True, related_name="posts")
+                              null=True, related_name="posts", verbose_name=
+                              "Группа")
 
     def __str__(self):
         return self.text
