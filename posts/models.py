57b27f0993a707b3b97ad80d80b2b5e3febe19df
Михаил <Mikevlg34@yandex.ru>
2021-01-24 23:36:21 +0400

    This is the way

--- posts/models.py
+++ posts/models.py
@@ -10,8 +10,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="posts", verbose_name="Автор")
     group = models.ForeignKey('Group', on_delete=models.SET_NULL, blank=True,
-                              null=True, related_name="posts", verbose_name=
-                              "Группа")
+                              null=True, related_name="posts",
+                              verbose_name="Группа")
 
     def __str__(self):
         return self.text
